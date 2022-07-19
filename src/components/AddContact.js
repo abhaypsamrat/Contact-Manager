76c9cfcd0e4d594163d81d3ad8b4bc388ac002e3
abhaypsamrat <abhay.p.samrat@gmail.com>
2022-07-19 08:48:37 +0530

    first commit

---
+++ src/components/AddContact.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class AddContact extends React.Component {
+  render() {
+    return (
+      <div className="ui main">
+        <h2>Add Contact</h2>
+      </div>
+    );
+  }
+}
+
+export default AddContact;
---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import AddContact from "./AddContact";
+import ContactList from "./ContactList";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      {/* <AddContact />
+      <ContactList /> */}
+    </div>
+  );
+}
+export default App;
